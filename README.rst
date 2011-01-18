4aa6eb3da4bf4dc900a50b3174a1c6edb3f3bdc2
James Socol <james@mozilla.com>
2011-01-18 18:49:03 -0500

    Congratulations, SphinxAPI in an installable package.

---
+++ README.rst
@@ -0,0 +1,5 @@
+==========
+Sphinx API
+==========
+
+See `the docs <http://sphinxsearch.com/docs/manual-0.9.9.html>`_.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='sphinxapi',
+    version='0.9.9',
+    description='Client for the Sphinx search engine.',
+    long_description=open('README.rst').read(),
+    author='Sphinx Technologies Inc',
+    url='http://github.com/jsocol/sphinxapi',
+    license='GPL',
+    packages='sphinxapi',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
